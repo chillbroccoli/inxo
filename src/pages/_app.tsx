f458c2b3bb7edb9486bfb553fb8529e40607ac65
Kacper Malek <kacper.malek0828@gmail.com>
2023-04-26 23:34:21 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "~/styles/globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+import { Raleway } from "next/font/google";
+import Head from "next/head";
+
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Inxo | Productivity App</title>
+      </Head>
+      <ClerkProvider {...pageProps}>
+        <div className={raleway.className}>
+          <Component {...pageProps} />
+        </div>
+      </ClerkProvider>
+    </>
+  );
 }
