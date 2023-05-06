b16991204ff85446fe2de88709289c69408f69bf
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-06 15:36:23 +0200

    use ToastProvider and QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import "~/styles/globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Raleway } from "next/font/google";
 import Head from "next/head";
+
+import { ToastProvider } from "~/components/ui/toast";
+import { queryClient } from "~/lib/query-client";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -14,9 +18,13 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Inxo | Productivity App</title>
       </Head>
       <ClerkProvider {...pageProps}>
-        <div className={raleway.className}>
-          <Component {...pageProps} />
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <ToastProvider swipeDirection="right">
+            <div className={raleway.className}>
+              <Component {...pageProps} />
+            </div>
+          </ToastProvider>
+        </QueryClientProvider>
       </ClerkProvider>
     </>
   );
