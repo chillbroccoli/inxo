6493460eef5a663ef32548f6e02ecfa45d4ead64
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-06 15:35:02 +0200

    add CreateTaskModal

---
+++ src/components/modals/CreateTaskModal.tsx
@@ -0,0 +1,94 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SelectValue } from "@radix-ui/react-select";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+
+import { createTask } from "~/lib/api";
+import { queryClient } from "~/lib/query-client";
+import { CreateTaskInput, createTaskSchema } from "~/lib/schemas/create-task.schema";
+
+import { Input } from "../atoms/Input";
+import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { SelectContent, SelectItem, SelectTrigger } from "../ui/select";
+
+export function CreateTaskModal() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const methods = useForm<CreateTaskInput>({
+    resolver: zodResolver(createTaskSchema),
+    defaultValues: {
+      dueDate: new Date(),
+    },
+  });
+  const { handleSubmit, reset } = methods;
+
+  const { mutateAsync, isLoading } = useMutation({
+    mutationFn: createTask,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(["paginatedTasks"]);
+      setIsModalOpen(false);
+      reset();
+    },
+  });
+
+  const onSubmit = async (data: CreateTaskInput) => {
+    await mutateAsync(data);
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DialogTrigger asChild>
+        <Button>Create New</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Task</DialogTitle>
+        </DialogHeader>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              <div className="grid w-full items-center gap-1.5">
+                <Input.Text name="name" label="Name" placeholder="Tasks name..." />
+              </div>
+              <div className="grid w-full items-center gap-1.5">
+                <Input.Select name="priority" label="Priority">
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select priority" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="LOW">Low</SelectItem>
+                    <SelectItem value="MEDIUM">Medium</SelectItem>
+                    <SelectItem value="HIGH">High</SelectItem>
+                  </SelectContent>
+                </Input.Select>
+              </div>
+              <div className="grid w-full items-center gap-1.5">
+                <Input.Select name="status" label="Status">
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="TODO">Todo</SelectItem>
+                    <SelectItem value="IN_PROGRESS">In Progress</SelectItem>
+                    <SelectItem value="DONE">Done</SelectItem>
+                  </SelectContent>
+                </Input.Select>
+              </div>
+              <div className="grid w-full items-center gap-1.5">
+                <Input.DatePicker name="dueDate" label="Due Date" />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-end mt-6">
+              <Button type="submit" isLoading={isLoading}>
+                Create
+              </Button>
+            </div>
+          </form>
+        </FormProvider>
+      </DialogContent>
+    </Dialog>
+  );
+}
