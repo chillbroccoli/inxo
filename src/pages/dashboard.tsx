76edea9c8d5a1a7855689a6b149692d86bee3588
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-06 15:36:32 +0200

    refactor

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,27 +1,5 @@
-import { GetServerSideProps } from "next";
-
-import { getXataClient, Tasks } from "~/lib/xata";
 import { TasksView } from "~/views/Tasks.view";
 
-const xata = getXataClient();
-
-export default function Home({ data }: { data: Tasks[] }) {
-  return <TasksView data={data} />;
+export default function Home() {
+  return <TasksView />;
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await xata.db.tasks.getPaginated({
-    pagination: {
-      size: 15,
-    },
-  });
-
-  return {
-    props: {
-      data: data.records.map((record) => ({
-        ...record,
-        dueDate: record.dueDate.toISOString(),
-      })),
-    },
-  };
-};
