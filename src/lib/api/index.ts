d7d7d955709843dfef3f07d10290598d8d347e54
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-08 18:45:00 +0200

    Update index.ts

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,14 +1,1 @@
-import ky from "ky";
-
-import { CreateTaskInput } from "../schemas/create-task.schema";
-import { Tasks } from "../xata";
-
-export async function getPaginatedTasks(): Promise<Tasks[]> {
-  const res = await ky.get("/api/tasks");
-  return res.json();
-}
-
-export async function createTask(data: CreateTaskInput) {
-  const res = await ky.post("/api/tasks", { json: data });
-  return res.json();
-}
+export { task } from "./task";
