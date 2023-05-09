32e3bc5c4daaba40f4811163488a2ff74ae41123
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-09 21:39:51 +0200

    add options constant file

---
+++ src/lib/constants/options.ts
@@ -0,0 +1,11 @@
+export const PRIORITY_OPTIONS = [
+  { value: "LOW", label: "Low" },
+  { value: "MEDIUM", label: "Medium" },
+  { value: "HIGH", label: "High" },
+];
+
+export const STATUS_OPTIONS = [
+  { value: "TODO", label: "Todo" },
+  { value: "IN_PROGRESS", label: "In Progress" },
+  { value: "DONE", label: "DONE" },
+];
