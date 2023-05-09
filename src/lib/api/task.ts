7a243da3bd76766b31199655857ed2473ff1a9b9
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-09 21:39:43 +0200

    use APIRoutes for routes strings

--- src/lib/api/task.ts
+++ src/lib/api/task.ts
@@ -1,16 +1,17 @@
 import ky from "ky";
 
+import { APIRoutes } from "../constants/routes";
 import { CreateTaskInput } from "../schemas/create-task.schema";
 import { Tasks } from "../xata";
 
 export const task = {
   async getAll(): Promise<Tasks[]> {
-    const res = await ky.get("/api/tasks");
+    const res = await ky.get(APIRoutes.TASKS);
     return res.json();
   },
 
   async create(data: CreateTaskInput) {
-    const res = await ky.post("/api/tasks", { json: data });
+    const res = await ky.post(APIRoutes.TASKS, { json: data });
     return res.json();
   },
 };
