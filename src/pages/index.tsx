0e11f335f6427ed421e3836ff3aaa179543c242e
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-13 19:48:10 +0200

    test toasts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Button } from "~/components/ui/button";
+import { useToast } from "~/components/ui/use-toast";
+
 export default function Home() {
+  const { toast } = useToast();
+
   return (
     <div>
       <h1>Hello World</h1>
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            description: "Your message has been sent.",
+          });
+        }}
+      >
+        Show Toast
+      </Button>
     </div>
   );
 }
